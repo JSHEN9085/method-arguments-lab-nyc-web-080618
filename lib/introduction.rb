758806910c007bb026e4886f5e6a861676aaff8b
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-24 00:55:37 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,4 @@
-require "spec_helper"
-
-describe "#introduction" do 
+def introduction do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
